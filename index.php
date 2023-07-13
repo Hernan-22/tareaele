a73c898c780cf51fbcf924bac6af1695ebfbef47
Hernán Barahona <BA16034@ues.edu.sv>
2023-07-13 06:42:43 -0600

    cambios

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Prueba de PHP</title>
+</head>
+<body>
+<?php echo '<p>Hola Mundo</p>'; ?>
+<h3>hola</h3>
+<p>Esto es un cambio de despligue continuo</p>
+<h1>hola</h1>
+</body>
+</html>
